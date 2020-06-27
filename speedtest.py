4e679102848a539cd4196af2115a17d419ddadb2
Hirokazu Chiba <hchiba1@gmail.com>
2020-06-28 06:26:26 +0900

    add --ambient option

--- speedtest.py
+++ speedtest.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 import sys
+import re
 import time
 import argparse
 import subprocess
+import requests
 
 parser = argparse.ArgumentParser(description='Repeat the Speedtest by Ookla')
 parser.add_argument('-n', '--num', default=0, type=int, help='limits the number of mesurements')
 parser.add_argument('-s', '--sec', default=0, type=int, help='sleep between tests (in seconds)')
 parser.add_argument('-H', '--header', action='store_true', help='output header line')
 parser.add_argument('--server', default='15047', help='server ID')
+parser.add_argument('--ambient', help='channel and key for Ambient service')
 args = parser.parse_args()
 
 command = [
@@ -19,6 +22,14 @@ command = [
 ]
 
 date_command = ['date', '+%F %T']
+
+channel, key = '', ''
+if args.ambient:
+    matched = re.search(r'^(\d+):([0-9a-z]+)$', args.ambient)
+    if not matched:
+        print('ERROR: invalid valud in --ambient')
+        sys.exit(1)
+    channel, key = matched.groups()
 
 def print_speed():
     ret = subprocess.run(command, stdout=subprocess.PIPE)
@@ -34,14 +45,21 @@ def print_speed():
     date_time = ret.stdout.decode().rstrip('\n')
 
     server_name, server_id, latency, jitter, packet_loss, download, upload, download_bytes, upload_bytes, share_url = fields
+    ping = f'{float(latency):.2f}'
     download_mbps = int(download) * 8 / 1000000
     upload_mbps = int(upload) * 8 / 1000000
     download_mb = int(download_bytes) / 1000000
     upload_mb = int(upload_bytes) / 1000000
-    print(f'{date_time} {float(latency):>6.2f} ms',
+    print(f'{date_time} {ping:>6} ms',
           f'{download_mbps:>7.2f} Mbit/s {upload_mbps:>7.2f} Mbit/s',
           f'{download_mb:>6.1f} MB {upload_mb:>6.1f} MB',
           f'{float(packet_loss):>4.1f}% {float(jitter):>6.2f} ms  {server_name}')
+
+    if args.ambient:
+        data = [{'created': date_time, 'd1': download_mbps, 'd2': upload_mbps, 'd3': ping}]
+        res = requests.post(f'http://ambidata.io/api/v2/channels/{channel}/dataarray', json={'writeKey': key, 'data': data})
+        if res.status_code != requests.codes.ok:
+            print('ERROR:', res.status_code, file=sys.stderr)
 
 def repeat_speed_test():
     count = 0
