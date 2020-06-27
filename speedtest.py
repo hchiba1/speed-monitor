aa0e4029f16a3019ca1e570dba5029c9bce3d03a
Hirokazu Chiba <hchiba1@gmail.com>
2020-06-27 16:35:39 +0900

    fix unit

--- speedtest.py
+++ speedtest.py
@@ -35,8 +35,8 @@ def print_speed():
     server_name, server_id, latency, jitter, packet_loss, download, upload, download_bytes, upload_bytes, share_url = fields
     download_mbps = int(download) * 8 / 1000000
     upload_mbps = int(upload) * 8 / 1000000
-    download_mb = int(download_bytes) / 1024 / 1024
-    upload_mb = int(upload_bytes) / 1024 / 1024
+    download_mb = int(download_bytes) / 1000000
+    upload_mb = int(upload_bytes) / 1000000
     print(f'{date_time} {float(latency):>6.2f} ms',
           f'{download_mbps:>7.2f} Mbit/s {upload_mbps:>7.2f} Mbit/s',
           f'{download_mb:>6.1f} MB {upload_mb:>6.1f} MB',
